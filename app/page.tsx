6b5bc64070220c7784c0da376ef61e1b2beb6101
Robert <robertmaximus383@gmail.com>
2023-07-20 22:10:38 +0300

    initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <p className='text-6xl text-green-500'>Basic Files</p>
+    <Button variant="destructive">Click</Button>
   )
 }
