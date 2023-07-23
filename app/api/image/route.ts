6c429ee952d35fc64b13adceb347d76164ef882a
Robert <robertmaximus383@gmail.com>
2023-07-23 12:03:27 +0300

    limit for every API added

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import { Configuration, OpenAIApi } from "openai";
 
+import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
+
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
@@ -36,11 +38,19 @@ export async function POST(
             return new NextResponse("Resolution is required", { status: 400})
         }
 
+        const freeTrail = await checkApiLimit();
+
+        if (!freeTrail) {
+            return new NextResponse("Free trail limit reached", { status: 403});
+        }
+
         const response = await openai.createImage({
             prompt,
             n: parseInt(amount, 10),
             size: resolution,
         })
+
+        await increaseApiLimit();
 
         return NextResponse.json(response.data.data)
     } catch (error) {
