8566ced6ca11e378a3b6f32ab616e2229675788e
Robert <robertmaximus383@gmail.com>
2023-07-26 18:36:32 +0300

    landing navbar check if user is already login

--- components/Landing-navbar.tsx
+++ components/Landing-navbar.tsx
@@ -33,7 +33,7 @@ export const LandingNavbar = () => {
             <div className="flex items-center gap-x-2">
                 <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
                     <Button variant="outline" className="rounded-full">
-                        Get Started
+                        {isSignedIn ? "Dashboard" : "Get Started"}
                     </Button>
                 </Link>
             </div>
